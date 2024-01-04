b1948b8d5712d995887f360cd2d81b4e8b487b6c
Okta P <57696739+oktaaaa@users.noreply.github.com>
2024-01-04 08:59:12 +0700

    education

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,20 @@ function App() {
   // const [cvName, setCvName] = useState([])
   // const [email, setEmail] = useState('')
 
+  // personal info
   const [pInfo, setPInfo] = useState({
     name: "", email: ""
   })
   const [cvPInfo, setCvPInfo] = useState([])
 
   let{name, email} = pInfo
+
+  const [eduInfo, setEduInfo] = useState({
+    year: "", place:"", description:""
+  })
+
+  const [cvEduInfo, setCvEduInfo] = useState([])
+  let{year, place, description} = eduInfo
   // function handleClick(e){
   //   e.preventDefault()
   //   setCvName(name)
@@ -29,12 +37,24 @@ function App() {
     })
   }
 
+  function handleChangeEdu(e){
+    setEduInfo({
+      ...eduInfo, [e.target.name]: e.target.value
+    })
+  }
+
   function handleClick(e){
     e.preventDefault()
     setCvPInfo([...cvPInfo, {name, email}])
-    console.log(pInfo);
+    // console.log(pInfo);
     setPInfo({name: '', email:''})
-    
+  }
+
+  function handleClickEdu(e){
+    e.preventDefault()
+    setCvEduInfo([...cvEduInfo, {year, place, description}])
+    // console.log(eduInfo);
+    setEduInfo({year: '', place: '', description:''})
   }
   // console.log("all", cvPInfo);
   return (
@@ -70,28 +90,114 @@ function App() {
                 />
               </div>
               
-              <button type="submit" className="btn btn-primary" onClick={handleClick}>Submit</button>
+              <button type="submit" className="btn btn-primary" onClick={handleClick}>Add</button>
+            </form>
+
+            {/* add this when all the data is good */}
+            {/* <div className="card text-center">
+              <div className="card-header">
+                <ul className="nav nav-tabs card-header-tabs">
+                  <li className="nav-item">
+                    <a className="nav-link active" href="#">Active</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">Link</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link disabled" href="#">Disabled</a>
+                  </li>
+                </ul>
+              </div>
+              <div className="card-body">
+                <h5 className="card-title">Special title treatment</h5>
+                <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" className="btn btn-primary">Go somewhere</a>
+              </div>
+            </div> */}
+
+            <h4 className='mt-3'>Education</h4>
+            <form >
+              <div className="form-group">
+                <label>Year</label>
+                <input type="text" className="form-control" placeholder=""
+                onChange={handleChangeEdu}
+                value={eduInfo.year}
+                name="year"
+                />
+              </div>
+              <div className="form-group">
+                <label>University Name</label>
+                <input type="text" className="form-control" placeholder=""
+                onChange={handleChangeEdu}
+                value={eduInfo.place}
+                name = "place"
+                />
+              </div>
+
+              <div className="form-group">
+                <label>Description</label>
+                <input type="text" className="form-control" placeholder=""
+                onChange={handleChangeEdu}
+                value={eduInfo.description}
+                name = "description"
+                />
+              </div>
+              
+              <button type="submit" className="btn btn-primary" onClick={handleClickEdu}>Add</button>
             </form>
           </div>
 
+          
+
           <div className="card col-lg-8">
-            Name is
-           
             {cvPInfo.map(
               (item, index)=>{
                 return(
                   <>
-                  <h3 key={index}>{item.name}</h3>
-                <h3>{item.email}</h3>
+                  <div >
+                      <h3 key={index}>{item.name} </h3>
+                      <p>{item.email} email here</p>
+                      <p>-----------------------------</p>
+                  </div>
+                  
                   </>
                 
                 )
               }
             )}
+
+          <h4>Education</h4>
+
+          {cvEduInfo.map(
+              (item, index)=>{
+                return(
+                  <>
+                  <div className='row'>
+                    <div className='col-lg-4'>
+                      <h3 key={index}>{item.year} </h3>
+                      
+                    </div>
+                    <div className='col-lg-8'>
+                      <h3 key={index}>{item.description} </h3>
+                      
+                    </div>
+                  </div>
+                  
+                  </>
+                
+                )
+              }
+            )}
+
+
             
           </div>
+
+          
           
         </div>
+
+        
       </div>
     </>
   )
